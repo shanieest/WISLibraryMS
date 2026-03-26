22649fa8aa0bd2bd9697a0b11b3c9f2794753adc
Jaja <160374912+shanieest@users.noreply.github.com>
2026-03-26 14:25:45 +0800

    WIP

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import SearchBar from './SearchBar';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
   const navigate = useNavigate();
@@ -11,16 +11,23 @@ export default function Header() {
   return (
     <header className="shadow-md">
       <div className="container mx-auto flex items-center justify-between py-4">
+        
         <SearchBar />
-        <div>
-          <button
-            type="button"
-            onClick={handleLoginClick} 
-            className="bg-[#ff7c08] text-white px-4 py-2 rounded hover:bg-[#e66e07] transition-colors"
-          >
-            Login
-          </button>
-        </div>
+
+        <nav className="flex gap-6">
+          <Link to="/home">Home</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/contact">Contact Us</Link>
+        </nav>
+
+        <button
+          type="button"
+          onClick={handleLoginClick}
+          className="bg-[#ff7c08] text-white px-4 py-2 rounded hover:bg-[#e66e07] transition-colors"
+        >
+          Login
+        </button>
+
       </div>
     </header>
   );
--- src/components/SectionWis.tsx
+++ src/components/SectionWis.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/Westfields Badge.png";
 
 export default function SectionWis() {
